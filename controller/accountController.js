f75b14d5e1a9734e1e03c9e57d06203784279223
DannyFillerSD <tuanvakhoi2991@gmail.com>
2024-07-21 17:18:13 +0700

    updateUser

--- controller/accountController.js
+++ controller/accountController.js
@@ -34,9 +34,9 @@ const AccountController ={
     },
     editAcount: async (req, res) => {
         try {
-          const accountUpdate = await Account.findById(req.params.id);
-          await accountUpdate.updateOne({ $set: req.body });
-          res.status(200).json(accountUpdate);
+            const accountUpdate = await Account.findOne({ Email: req.params.email });
+            await accountUpdate.updateOne({ $set: req.body });
+          res.status(200).json("update thành công");
         } catch (error) {
           res.status(200).json(error);
         }
--- router/accountRouter.js
+++ router/accountRouter.js
@@ -4,5 +4,6 @@ const router = require("express").Router();
 router.get("/",AccountController.getAllAccount);
 router.post("/",AccountController.postAccount);
 router.get("/:email",AccountController.getAccount);
+router.put("/:email",AccountController.editAcount);
 
 module.exports = router;
